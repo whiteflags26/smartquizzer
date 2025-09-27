102b6df9fe36e0324672683d8ab1b4e02a160a7c
codenim34 <tasnim.iterator34@gmail.com>
2025-09-27 08:19:56 +0600

    Add initial implementation of ConfigServer and User model

---
+++ QuestionService/src/main/java/com/whiteflags26/smartquizzer/auth/model/User.java
@@ -0,0 +1,17 @@
+package com.whiteflags26.smartquizzer.auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private Integer xp;
+    // Add any other fields needed
+}
